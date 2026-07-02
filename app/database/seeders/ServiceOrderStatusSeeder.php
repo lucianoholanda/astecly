2c82cd704dd3dfbf799ab8142120f7b37a9cba00
Luciano Holanda <lucianoholanda@gmail.com>
2026-07-02 20:38:43 -0300

    Atualização do seeder

--- app/database/seeders/ServiceOrderStatusSeeder.php
+++ app/database/seeders/ServiceOrderStatusSeeder.php
@@ -10,12 +10,17 @@ class ServiceOrderStatusSeeder extends Seeder
     public function run(): void
     {
         $statuses = [
-            ['name' => 'Orçamento Pendente', 'color' => 'warning', 'is_default' => true],
-            ['name' => 'Aprovado - Aguardando Peça', 'color' => 'info', 'is_default' => false],
-            ['name' => 'Em Manutenção na Bancada', 'color' => 'primary', 'is_default' => false],
+            ['name' => 'Em Análise', 'color' => 'warning', 'is_default' => true],
+            ['name' => 'Aguardando Coleta', 'color' => 'warning', 'is_default' => false],
+            ['name' => 'Aguardando Visita', 'color' => 'warning', 'is_default' => false],
+            ['name' => 'Aguardando Peça', 'color' => 'info', 'is_default' => false],
+            ['name' => 'Em Manutenção', 'color' => 'primary', 'is_default' => false],
             ['name' => 'Aguardando Retirada', 'color' => 'success', 'is_default' => false],
-            ['name' => 'Entregue / Finalizado', 'color' => 'gray', 'is_default' => false],
-            ['name' => 'Orçamento Recusado', 'color' => 'danger', 'is_default' => false],
+            ['name' => 'Retirado', 'color' => 'gray', 'is_default' => false],
+            ['name' => 'Entregue', 'color' => 'gray', 'is_default' => false],
+            ['name' => 'Não Aprovado', 'color' => 'danger', 'is_default' => false],
+            ['name' => 'Condenado', 'color' => 'danger', 'is_default' => false],
+            ['name' => 'Descarte', 'color' => 'danger', 'is_default' => false],
         ];
 
         foreach ($statuses as $status) {
